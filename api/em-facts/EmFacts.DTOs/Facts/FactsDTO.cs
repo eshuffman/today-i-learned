33be67afc30c405244b4938b19f1e7df24242f01
Emily Huffman <ehuffman@catalyte.io>
2023-02-02 14:41:15 -0600

    DTO and model updated with line items for imgsrc, date created, and date reviewed

--- api/em-facts/EmFacts.DTOs/Facts/FactsDTO.cs
+++ api/em-facts/EmFacts.DTOs/Facts/FactsDTO.cs
@@ -1,4 +1,9 @@
-﻿namespace EmFacts.DTOs
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmFacts.DTOs
 {
     /// <summary>
     /// Describes a data transfer object for a fact.
@@ -9,10 +14,18 @@
 
         public string Question { get; set; }
 
+        public string ImageSrc { get; set; }
+
         public string Tidbit { get; set; }
 
         public string[] Tags { get; set; }
 
         public int Tier { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime ReviewDate { get; set; }
     }
 }
+
+//future feature: ability to store each time reviewed and how review went
--- api/em-facts/EmFacts.Data/Model/Fact.cs
+++ api/em-facts/EmFacts.Data/Model/Fact.cs
@@ -1,4 +1,9 @@
-﻿namespace EmFacts.Data.Model
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmFacts.Data.Model
 {
     /// <summary>
     /// Describes a promo code for transaction.
@@ -7,10 +12,18 @@
     {
         public string Question { get; set; }
 
+        public string ImageSrc { get; set; }
+
         public string Tidbit { get; set; }
 
         public string[] Tags { get; set; }
 
         public int Tier { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CreationDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReviewDate { get; set; }
     }
 }
