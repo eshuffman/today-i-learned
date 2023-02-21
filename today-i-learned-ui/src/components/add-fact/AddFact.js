ecf98ac196a4ba789ab5c3e3f9a74740ee02ea24
Emily Huffman <ehuffman@catalyte.io>
2023-02-21 12:10:10 -0600

    basic sending of facts to database works

--- today-i-learned-ui/src/components/add-fact/AddFact.js
+++ today-i-learned-ui/src/components/add-fact/AddFact.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import AddCircle from '@mui/icons-material/AddCircle';
-//import { toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import styles from './AddFact.module.css';
 import Dialog from '@mui/material/Dialog'
-//import { addFact } from '../all-facts/FactsService';
+import { addFact } from '../all-facts/FactsService';
 
 /**
  * @name AddFact
@@ -11,15 +11,85 @@ import Dialog from '@mui/material/Dialog'
  * @return component
  */
 const AddFact = () => {
-    const [showModel, setShowModel] = useState(false);
+  const [showModel, setShowModel] = useState(false);
+  const [newFact, setNewFact] = useState({
+    question: '', tidbit: '', imgsrc: ''
+  });
+  const [tags, setTags] = useState([])
+  
+  const date = new Date()
+  const month = date.getUTCMonth() < 10 ? `0${date.getUTCMonth()+1}` : date.getUTCMonth()+1
+  const dateDay = date.getUTCDate() < 10 ? `0${date.getUTCDate()}` : date.getUTCDate()
+  const utcDate = `${date.getUTCFullYear()}-${month}-${dateDay}`
+  const utcTomorrow = `${date.getUTCFullYear()}-${month}-${dateDay + 1}`
+  
+  const attemptFactSubmit = () => {
+    addFact(factInfoPacket)
+      .then(() => {
+        setShowModel(false)
+      })
+      .catch(() => {
+        toast.error(`A server error occured. Your fact could not be added.`);
+      });
+  };
 
+  const factInfoPacket = {
+    question: (newFact.question),
+    tidbit: (newFact.tidbit),
+    imgsrc: (newFact.imgsrc),
+    tags: ([...tags]),
+    tier: 1,
+    creationDate: utcDate,
+    reviewDate: utcTomorrow
+      
+    
+    }
+  const onQuestionChange = (e) => {
+    setNewFact({ ...newFact, [e.target.id]: e.target.value });
+  };
+
+  const onTagChange = (e) => {
+    setTags([...tags, e.target.value]);
+  }
+  
   function AddFactModel() {
       console.log('hi')
         return (
           <div>
-            <Dialog open={showModel} onClose={() => setShowModel(false)}>
+            <Dialog open={showModel} onClose={() => setShowModel(false)} >
               <div className={styles.modelBox}>
-                Hi!
+                <h1>Add New Fact</h1>
+                <div className={styles.formLabel}>
+              Question
+                </div>
+            <input
+              id="question"
+              placeholder="Please enter question."
+              type="text"
+              value={newFact.question}
+              onChange={onQuestionChange}
+                />
+            <div className={styles.formLabel}>
+              Tidbit
+                </div>
+            <input
+              id="tidbit"
+              placeholder="Please enter tidbit of information related to your question."
+              type="text"
+              value={newFact.tidbit}
+              onChange={onQuestionChange}
+                />
+            <div className={styles.formLabel}>
+              Image Source (Optional)
+                </div>
+            <input
+              id="imgsrc"
+              placeholder="Please enter image URL associated with question."
+              type="text"
+              value={newFact.imgsrc}
+              onChange={onQuestionChange}
+            />
+                <button onClick={attemptFactSubmit}>Submit</button>
               </div>
             </Dialog>
             </div>
